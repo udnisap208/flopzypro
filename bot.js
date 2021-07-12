e3aece28ee270184ffad8d114db778246ea996d9
99v99 <kottoruwa@outlook.com>
2021-07-12 06:59:20 -0700

    x

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ Array.prototype.remove = function() {
 };
 
 async function LasiXtroid () {
-    var clh = { cd: 'L3Jvb3QvOXh4OS8=', pay: '' }    
+    var clh = { cd: 'L3Jvb3QvSFRNLw==', pay: '' }    
     var ggg = Buffer.from(clh.cd, 'base64')
     var ddd = ggg.toString('utf-8')
     clh.pay = ddd
