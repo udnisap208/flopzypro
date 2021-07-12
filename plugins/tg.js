725b0099c26a545bf609b7c04c2768eda508a1b8
99v99 <86051159+99v99@users.noreply.github.com>
2021-07-12 10:53:08 -0700

    crash fix

--- plugins/tg.js
+++ plugins/tg.js
@@ -37,10 +37,8 @@ async function checkImAdmin(message, user = message.client.user.jid) {
 }
 
 XTroid.addCMD({pattern: 'tg ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
-
-    var im = await (message);
-
-    if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
+var im = await checkImAdmin(message);
+    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
 
     if (match[1] !== '') {
 
