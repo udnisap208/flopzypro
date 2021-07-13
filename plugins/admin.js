719685049a551b04094e9fe90361f1004b3857fd
99v99 <86051159+99v99@users.noreply.github.com>
2021-07-13 08:36:42 -0700

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-X-Troid - Yusuf Usta
+WhatsAsena - Yusuf Usta
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
