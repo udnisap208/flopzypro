4294e39f7cdd0b79565697d1cbc559e932d938f0
99v99 <86051159+99v99@users.noreply.github.com>
2021-07-18 05:08:51 -0700

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -1,3 +1,10 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
 
 const XTroid = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
@@ -15,9 +22,32 @@ const ALIMGOF = "already off : කලින්ම වසාදමා ඇත"
 const SUON = "Successfully .img on command: සාර්ථකව අවසර ලබාදෙන ලදී "
 const SUOF = "Successfully .img off command: සාර්ථකව අවහිර කරන ලදී "
 const IMDC = "you tuerned off .img command plz tuern on *.cimg on*"
-let baseURI = '/apps/' + config.HEROKU.APP_NAME;const axios = require('axios');
+let baseURI = '/apps/' + config.HEROKU.APP_NAME;
+//============================== LYRICS =============================================
+const axios = require('axios');
 const { requestLyricsFor, requestAuthorFor, requestTitleFor, requestIconFor } = require("solenolyrics");
 const solenolyrics= require("solenolyrics"); 
+//============================== CURRENCY =============================================
+const { exchangeRates } = require('exchange-rates-api');
+const ExchangeRatesError = require('exchange-rates-api/src/exchange-rates-error.js')
+//============================== TTS ==================================================
+const fs = require('fs');
+const https = require('https');
+const googleTTS = require('google-translate-tts');
+//=====================================================================================
+//============================== YOUTUBE ==============================================
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const yts = require( 'yt-search' )
+const got = require("got");
+const ID3Writer = require('browser-id3-writer');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
+    clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009'
+});
+//=====================================================================================
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
 const Glang = Language.getString('github');
@@ -26,49 +56,51 @@ const Clang = Language.getString('covid');
 
 const wiki = require('wikijs').default;
 var gis = require('g-i-s');
-const { on } = require('events');
 
-if (config.IMG == 'on') {
+
+
 if (config.WORKTYPE == 'private') {
-XTroid.addCMD({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
-    
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
-    gis(match[1], async (error, result) => {
-        for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
-            var get = got(result[i].url, {https: {rejectUnauthorized: false}});
-            var stream = get.buffer();
-            
-            stream.then(async (image) => {
-                await message.client.sendMessage(message.jid,image, MessageType.image);
-            });
-        }
 
-        message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
-    });
-}));
+    XTroid.addCMD({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
+        if (config.IMG !== true) return await message.sendMessage(IMDC);
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
+        gis(match[1], async (error, result) => {
+            for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
+                var get = got(result[i].url, {https: {rejectUnauthorized: false}});
+                var stream = get.buffer();
+                
+                stream.then(async (image) => {
+                    await message.client.sendMessage(message.jid,image, MessageType.image);
+                });
+            }
+
+            message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
+        });
+    }));
+
 }
-
-
-
 else if (config.WORKTYPE == 'public') {
-XTroid.addCMD({pattern: 'img ?(.*)', fromMe: false,desc: Lang.IMG_DESC }, (async (message, match) => { 
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
-    gis(match[1], async (error, result) => {
-        for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
-            var get = got(result[i].url, {https: {rejectUnauthorized: false}});
-            var stream = get.buffer();
-            
-            stream.then(async (image) => {
-                await message.client.sendMessage(message.jid,image, MessageType.image);
-            });
-        }
+  
+   
+    XTroid.addCMD({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (async (message, match) => {
+        if (config.IMG !== true) return await message.sendMessage(IMDC);
 
-        message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
-    });
-}));
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
+        gis(match[1], async (error, result) => {
+            for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
+                var get = got(result[i].url, {https: {rejectUnauthorized: false}});
+                var stream = get.buffer();
+                
+                stream.then(async (image) => {
+                    await message.client.sendMessage(message.jid,image, MessageType.image);
+                });
+            }
 
+            message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
+        });
+    }));
+    
 }
-}
-
