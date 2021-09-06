1c5209fc702acd6d754b377cd684b8ec03817b61
chopaya <66366054+udnisap208@users.noreply.github.com>
2021-09-06 16:58:24 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -87,8 +87,44 @@ XTroid.addCMD({ pattern: 'gsmarena ?(.*)', fromMe: true, desc: Lang.DESC }, (asy
 }
 else if (Config.WORKTYPE == 'public') {
 
-   
-   XTroid.addCMD({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
+XTroid.addCMD({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
+        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
+	let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`
+	const response = await got(url);
+	const lasijson = JSON.parse(response.body);
+	
+	let msg = '';
+	msg += '*Username:*\n' + '```' + lasijson.username + '```' + '\n\n';
+	msg += '*Full Name:*\n' + '```' + lasijson.fullname + '```' + '\n\n';
+	msg += '*Verified:*\n' + '```' + lasijson.verified + '```' + '\n\n';
+	msg += '*Bio:*\n' + '```' + lasijson.bio + '```' + '\n\n';
+	msg += '*Followers:*\n' + '```' + lasijson.followers + '```' + '\n\n';
+	msg += '*Following:*\n' + '```' + lasijson.follow + '```' + '\n\n';
+	
+
+	var lasiimage = await axios.get(lasijson.thumbnail, { responseType: 'arraybuffer' })
+	
+	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
+}));	
+	
+	XTroid.addCMD({ pattern: 'gsmarena ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
+        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
+	let url = `https://docs-jojo.herokuapp.com/api/gsm_arena?q=${match[1]}`
+	const response = await got(url);
+	const lasijson = JSON.parse(response.body);
+	
+	let msg = '';
+	msg += '*Title:*\n' + '```' + lasijson.title + '```' + '\n\n';
+	msg += '*Release:*\n' + '```' + lasijson.released + '```' + '\n\n';
+	msg += '*OS:*\n' + '```' + lasijson.os_version + '```' + '\n\n';
+	msg += '*Specs:*\n' + '```' + lasijson.spec.list + '```' + '\n\n';	
+
+	var lasiimage = await axios.get(lasijson.img, { responseType: 'arraybuffer' })
+	
+	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
+}));
+
+	XTroid.addCMD({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`
 	const response = await got(url);
@@ -107,24 +143,7 @@ else if (Config.WORKTYPE == 'public') {
 	
 	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
 }));
-
-	XTroid.addCMD({ pattern: 'gsmarena ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
-	let url = `https://docs-jojo.herokuapp.com/api/gsm_arena?q=${match[1]}`
-	const response = await got(url);
-	const lasijson = JSON.parse(response.body);
 	
-	let msg = '';
-	msg += '*Title:*\n' + '```' + lasijson.title + '```' + '\n\n';
-	msg += '*Release:*\n' + '```' + lasijson.released + '```' + '\n\n';
-	msg += '*OS:*\n' + '```' + lasijson.os_version + '```' + '\n\n';
-	msg += '*Specs:*\n' + '```' + lasijson.spec.list + '```' + '\n\n';	
-
-	var lasiimage = await axios.get(lasijson.img, { responseType: 'arraybuffer' })
-	
-	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
-}));
-
     /*
     XTroid.addCMD({ pattern: 'tiktok ?(.*)', fromMe: false, desc: Tlang.TİKTOK }, async (message, match) => {
 
