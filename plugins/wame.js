b0b830b4e18585e2c97d4c693f15c7e7010852c9
chopaya <66366054+udnisap208@users.noreply.github.com>
2021-09-11 14:01:02 +0530

    Update wame.js

--- plugins/wame.js
+++ plugins/wame.js
@@ -22,3 +22,19 @@ const NEED_UWONG = "*Give me a user!*"
             await message.client.sendMessage(message.jid, NEED_UWONG, MessageType.text);
         }
     }));
+ 
+    XTroid.addCMD({pattern: 'wame ?(.*)', fromMe: false, desc: WAME_DESC}, (async (message, match) => {    
+        if (message.reply_message !== false) {
+            await message.client.sendMessage(message.jid, WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid.replace('@s.whatsapp.net', ' ')), MessageType.text, {
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+            });
+        } else if (message.mention !== false) {
+            message.mention.map(async user => {
+                await message.client.sendMessage(message.jid, WAME.format(user.split('@')[0], user.replace('@s.whatsapp.net', ' ')), MessageType.text, {
+                    contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
+                }); 
+            });
+        } else {
+            await message.client.sendMessage(message.jid, NEED_UWONG, MessageType.text);
+        }
+    }));
