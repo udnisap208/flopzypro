ce3ce70ac940eacc116dea113152f21283c30707
chopaya <66366054+udnisap208@users.noreply.github.com>
2021-10-26 17:30:11 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -3,58 +3,94 @@ Telegram: t.me/phaticusthiccy
 Instagram: www.instagram.com/kyrie.baran
 */
 
-const XTroid = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const got = require('got');
-const Config = require('../config');
-const axios = require('axios');
-const fs = require('fs')
+const XTroid = require("../events");
+const {
+  MessageType,
+  MessageOptions,
+  Mimetype,
+} = require("@adiwajshing/baileys");
+const got = require("got");
+const Config = require("../config");
+const axios = require("axios");
+const fs = require("fs");
 
-const Language = require('../language')
-const { errorMessage, infoMessage } = require('../helpers')
-const Lang = Language.getString('instagram')
-const Tlang = Language.getString('tiktok')
+const Language = require("../language");
+const { errorMessage, infoMessage } = require("../helpers");
+const Lang = Language.getString("instagram");
+const Tlang = Language.getString("tiktok");
 
-if (Config.WORKTYPE == 'private') {
+if (Config.WORKTYPE == "private") {
+  XTroid.addCMD(
+    { pattern: "insta ?(.*)", fromMe: true, desc: Lang.DESC },
+    async (message, match) => {
+      if (match[1] === "")
+        return await message.client.sendMessage(
+          message.jid,
+          "```Give me a name.```",
+          MessageType.text,
+          { quoted: message.data }
+        );
+      let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`;
+      const response = await got(url);
+      const lasijson = JSON.parse(response.body);
 
-    XTroid.addCMD({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
-	let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`
-	const response = await got(url);
-	const lasijson = JSON.parse(response.body);
-	
-	let msg = '';
-	msg += '*Username:*\n' + '```' + lasijson.username + '```' + '\n\n';
-	msg += '*Full Name:*\n' + '```' + lasijson.fullname + '```' + '\n\n';
-	msg += '*Verified:*\n' + '```' + lasijson.verified + '```' + '\n\n';
-	msg += '*Bio:*\n' + '```' + lasijson.bio + '```' + '\n\n';
-	msg += '*Followers:*\n' + '```' + lasijson.followers + '```' + '\n\n';
-	msg += '*Following:*\n' + '```' + lasijson.follow + '```' + '\n\n';
-	
+      let msg = "";
+      msg += "*Username:*\n" + "```" + lasijson.username + "```" + "\n\n";
+      msg += "*Full Name:*\n" + "```" + lasijson.fullname + "```" + "\n\n";
+      msg += "*Verified:*\n" + "```" + lasijson.verified + "```" + "\n\n";
+      msg += "*Bio:*\n" + "```" + lasijson.bio + "```" + "\n\n";
+      msg += "*Followers:*\n" + "```" + lasijson.followers + "```" + "\n\n";
+      msg += "*Following:*\n" + "```" + lasijson.follow + "```" + "\n\n";
 
-	var lasiimage = await axios.get(lasijson.thumbnail, { responseType: 'arraybuffer' })
-	
-	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
-}));
+      var lasiimage = await axios.get(lasijson.thumbnail, {
+        responseType: "arraybuffer",
+      });
 
-XTroid.addCMD({ pattern: 'gsmarena ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
-	let url = `https://docs-jojo.herokuapp.com/api/gsm_arena?q=${match[1]}`
-	const response = await got(url);
-	const lasijson = JSON.parse(response.body);
-	
-	let msg = '';
-	msg += '*Title:*\n' + '```' + lasijson.title + '```' + '\n\n';
-	msg += '*Release:*\n' + '```' + lasijson.released + '```' + '\n\n';
-	msg += '*OS:*\n' + '```' + lasijson.os_version + '```' + '\n\n';
-	msg += '*Specs:*\n' + '```' + lasijson.spec.list + '```' + '\n\n';	
+      await message.client.sendMessage(
+        message.jid,
+        Buffer.from(lasiimage.data),
+        MessageType.image,
+        { mimetype: Mimetype.png, caption: msg }
+      );
+    }
+  );
 
-	var lasiimage = await axios.get(lasijson.img, { responseType: 'arraybuffer' })
-	
-	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
-}));
+  XTroid.addCMD(
+    { pattern: "device ?(.*)", fromMe: true, desc: Lang.DESC },
+    async (message, match) => {
+      const { data } = await axios(
+        `https://zenzapi.xyz/api/gsmarena?query=${match[1]}&apikey=a9a05974d30e`
+      );
+      const { status, result } = data;
+      if (!status) return await message.sendMessage("not found");
+      await message.client.sendMessage(
+        message.jid,
+        "_Fetching Data_",
+        MessageType.text,
+        { quoted: message.data }
+      );
+      let msg = "```";
+      msg += `NAME :${result.judul}\n\n`;
+      msg += `DATE :${result.rilis}\n\n`;
+      msg += `CPU :${result.chipset}\n\n`;
+      msg += `VERSION :${result.type}\n\n`;
+      msg += `RAM :${result.ram}\n\n`;
+      msg += `ROM :${result.storage}\n\n`;
+      msg += `DISPLAY :${result.display}\n\n`;
+      msg += `CAMERA MP :${result.pixel}\n\n`;
+      msg += `BATTERY :${result.batrai}\n\n`;
+      msg += `INCH :${result.inchi}\n\n`;
+      msg += `VIDEO MP :${result.videoPixel}\n\n`;
+      msg += `WEIGHT :${result.ukuran}\n\n`;
+      msg += "```";
+      
+      var lasiimage = await axios.get(`${result.thumb}`, { responseType: 'arraybuffer' })
 
-    /*
+      await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
+    }
+  );
+
+  /*
     XTroid.addCMD({ pattern: 'tiktok ?(.*)', fromMe: true, desc: Tlang.TİKTOK }, async (message, match) => {
 
         const userName = match[1]
@@ -84,67 +120,150 @@ XTroid.addCMD({ pattern: 'gsmarena ?(.*)', fromMe: true, desc: Lang.DESC }, (asy
       },
     )
     */
-}
-else if (Config.WORKTYPE == 'public') {
+} else if (Config.WORKTYPE == "public") {
+  XTroid.addCMD(
+    { pattern: "insta ?(.*)", fromMe: false, desc: Lang.DESC },
+    async (message, match) => {
+      if (match[1] === "")
+        return await message.client.sendMessage(
+          message.jid,
+          "```Give me a name.```",
+          MessageType.text,
+          { quoted: message.data }
+        );
+      let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`;
+      const response = await got(url);
+      const lasijson = JSON.parse(response.body);
 
-XTroid.addCMD({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
-	let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`
-	const response = await got(url);
-	const lasijson = JSON.parse(response.body);
-	
-	let msg = '';
-	msg += '*Username:*\n' + '```' + lasijson.username + '```' + '\n\n';
-	msg += '*Full Name:*\n' + '```' + lasijson.fullname + '```' + '\n\n';
-	msg += '*Verified:*\n' + '```' + lasijson.verified + '```' + '\n\n';
-	msg += '*Bio:*\n' + '```' + lasijson.bio + '```' + '\n\n';
-	msg += '*Followers:*\n' + '```' + lasijson.followers + '```' + '\n\n';
-	msg += '*Following:*\n' + '```' + lasijson.follow + '```' + '\n\n';
-	
+      let msg = "";
+      msg += "*Username:*\n" + "```" + lasijson.username + "```" + "\n\n";
+      msg += "*Full Name:*\n" + "```" + lasijson.fullname + "```" + "\n\n";
+      msg += "*Verified:*\n" + "```" + lasijson.verified + "```" + "\n\n";
+      msg += "*Bio:*\n" + "```" + lasijson.bio + "```" + "\n\n";
+      msg += "*Followers:*\n" + "```" + lasijson.followers + "```" + "\n\n";
+      msg += "*Following:*\n" + "```" + lasijson.follow + "```" + "\n\n";
 
-	var lasiimage = await axios.get(lasijson.thumbnail, { responseType: 'arraybuffer' })
-	
-	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
-}));	
-	
-	XTroid.addCMD({ pattern: 'gsmarena ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
-	let url = `https://docs-jojo.herokuapp.com/api/gsm_arena?q=${match[1]}`
-	const response = await got(url);
-	const lasijson = JSON.parse(response.body);
-	
-	let msg = '';
-	msg += '*Title:*\n' + '```' + lasijson.title + '```' + '\n\n';
-	msg += '*Release:*\n' + '```' + lasijson.released + '```' + '\n\n';
-	msg += '*OS:*\n' + '```' + lasijson.os_version + '```' + '\n\n';
-	msg += '*Specs:*\n' + '```' + lasijson.list + '```' + '\n\n';	
+      var lasiimage = await axios.get(lasijson.thumbnail, {
+        responseType: "arraybuffer",
+      });
 
-	var lasiimage = await axios.get(lasijson.img, { responseType: 'arraybuffer' })
-	
-	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
-}));
+      await message.client.sendMessage(
+        message.jid,
+        Buffer.from(lasiimage.data),
+        MessageType.image,
+        { mimetype: Mimetype.png, caption: msg }
+      );
+    }
+  );
 
-	XTroid.addCMD({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
-	let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`
-	const response = await got(url);
-	const lasijson = JSON.parse(response.body);
-	
-	let msg = '';
-	msg += '*Username:*\n' + '```' + lasijson.username + '```' + '\n\n';
-	msg += '*Full Name:*\n' + '```' + lasijson.fullname + '```' + '\n\n';
-	msg += '*Verified:*\n' + '```' + lasijson.verified + '```' + '\n\n';
-	msg += '*Bio:*\n' + '```' + lasijson.bio + '```' + '\n\n';
-	msg += '*Followers:*\n' + '```' + lasijson.followers + '```' + '\n\n';
-	msg += '*Following:*\n' + '```' + lasijson.follow + '```' + '\n\n';
-	
+  XTroid.addCMD(
+    { pattern: "device ?(.*)", fromMe: true, desc: Lang.DESC },
+    async (message, match) => {
+      const { data } = await axios(
+        `https://zenzapi.xyz/api/gsmarena?query=${match[1]}&apikey=a9a05974d30e`
+      );
+      const { status, result } = data;
+      if (!status) return await message.sendMessage("not found");
+      await message.client.sendMessage(
+        message.jid,
+        "_Fetching Data_",
+        MessageType.text,
+        { quoted: message.data }
+      );
+      let msg = "```";
+      msg += `NAME :${result.judul}\n\n`;
+      msg += `DATE :${result.rilis}\n\n`;
+      msg += `CPU :${result.chipset}\n\n`;
+      msg += `VERSION :${result.type}\n\n`;
+      msg += `RAM :${result.ram}\n\n`;
+      msg += `ROM :${result.storage}\n\n`;
+      msg += `DISPLAY :${result.display}\n\n`;
+      msg += `CAMERA MP :${result.pixel}\n\n`;
+      msg += `BATTERY :${result.batrai}\n\n`;
+      msg += `INCH :${result.inchi}\n\n`;
+      msg += `VIDEO MP :${result.videoPixel}\n\n`;
+      msg += `WEIGHT :${result.ukuran}\n\n`;
+      msg += "```";
+      
+      var lasiimage = await axios.get(`${result.thumb}`, { responseType: 'arraybuffer' })
 
-	var lasiimage = await axios.get(lasijson.thumbnail, { responseType: 'arraybuffer' })
-	
-	await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
-}));
-	
-    /*
+      await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
+    }
+  );
+  XTroid.addCMD(
+    { pattern: "device ?(.*)", fromMe: false, desc: Lang.DESC },
+    async (message, match) => {
+      const { data } = await axios(
+        `https://zenzapi.xyz/api/gsmarena?query=${match[1]}&apikey=a9a05974d30e`
+      );
+      const { status, result } = data;
+      if (!status) return await message.sendMessage("not found");
+      await message.client.sendMessage(
+        message.jid,
+        "_Fetching Data_",
+        MessageType.text,
+        { quoted: message.data }
+      );
+      let msg = "```";
+      msg += `NAME :${result.judul}\n\n`;
+      msg += `DATE :${result.rilis}\n\n`;
+      msg += `CPU :${result.chipset}\n\n`;
+      msg += `VERSION :${result.type}\n\n`;
+      msg += `RAM :${result.ram}\n\n`;
+      msg += `ROM :${result.storage}\n\n`;
+      msg += `DISPLAY :${result.display}\n\n`;
+      msg += `CAMERA MP :${result.pixel}\n\n`;
+      msg += `BATTERY :${result.batrai}\n\n`;
+      msg += `INCH :${result.inchi}\n\n`;
+      msg += `VIDEO MP :${result.videoPixel}\n\n`;
+      msg += `WEIGHT :${result.ukuran}\n\n`;
+      msg += "```";
+      
+      
+      
+      
+      var lasiimage = await axios.get(`${result.thumb}`, { responseType: 'arraybuffer' })
+
+      await message.client.sendMessage(message.jid, Buffer.from(lasiimage.data), MessageType.image, {mimetype: Mimetype.png, caption: msg });
+    }
+  );
+
+  XTroid.addCMD(
+    { pattern: "insta ?(.*)", fromMe: true, desc: Lang.DESC },
+    async (message, match) => {
+      if (match[1] === "")
+        return await message.client.sendMessage(
+          message.jid,
+          "```Give me a name.```",
+          MessageType.text,
+          { quoted: message.data }
+        );
+      let url = `https://hardianto-chan.herokuapp.com/api/igstalk?username=${match[1]}&apikey=hardianto`;
+      const response = await got(url);
+      const lasijson = JSON.parse(response.body);
+
+      let msg = "";
+      msg += "*Username:*\n" + "```" + lasijson.username + "```" + "\n\n";
+      msg += "*Full Name:*\n" + "```" + lasijson.fullname + "```" + "\n\n";
+      msg += "*Verified:*\n" + "```" + lasijson.verified + "```" + "\n\n";
+      msg += "*Bio:*\n" + "```" + lasijson.bio + "```" + "\n\n";
+      msg += "*Followers:*\n" + "```" + lasijson.followers + "```" + "\n\n";
+      msg += "*Following:*\n" + "```" + lasijson.follow + "```" + "\n\n";
+
+      var lasiimage = await axios.get(lasijson.thumbnail, {
+        responseType: "arraybuffer",
+      });
+
+      await message.client.sendMessage(
+        message.jid,
+        Buffer.from(lasiimage.data),
+        MessageType.image,
+        { mimetype: Mimetype.png, caption: msg }
+      );
+    }
+  );
+
+  /*
     XTroid.addCMD({ pattern: 'tiktok ?(.*)', fromMe: false, desc: Tlang.TİKTOK }, async (message, match) => {
 
         const userName = match[1]
