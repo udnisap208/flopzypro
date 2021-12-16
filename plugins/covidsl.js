38178b17c2e8919cef99d3368eaa566e21642429
chopaya <udnisap208@gmail.com>
2021-12-16 19:07:16 +0530

    Update covidsl.js

--- plugins/covidsl.js
+++ plugins/covidsl.js
@@ -1,10 +1,3 @@
-/* # Exclusively from danuma project 
-# Do not use this fore any commercial thing
-# If you abuse thais bot we wil kick you from bot 
-# Do not edit (Respect to the Devaoloper) 
-# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
-# Get more about devaoloper https://lasiya.ml
-*/
 
 const XTroid = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
